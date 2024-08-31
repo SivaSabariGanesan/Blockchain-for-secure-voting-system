6d79c8df04e09c87e688763b7edb3cee1ac8c81c
SivaSabariGanesan <154002297+SivaSabariGanesan@users.noreply.github.com>
2024-09-01 02:15:46 +0530

    Add files via upload

---
+++ logout.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const Logout: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    localStorage.removeItem("auth");
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, [router]);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-full text-2xl">
+      <h1>Thanks for Voting</h1>
+      <p>Return to landing page in 3 seconds...</p>
+    </div>
+  );
+};
+
+export default Logout;
